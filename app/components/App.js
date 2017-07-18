0ca8ee1cd920efc3c01c82e4f2b9a63e4933221d
Aline Pickler <aline.pickler@gmail.com>
2017-07-18 13:27:13 +0200

    Show posts

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
-import Post from './Post';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(result=>result.json())
+      .then(items=>this.setState({items}))
+  }
+  /*
+  getQueryParam(param) {
+  	var vars = {};
+  	window.location.href.replace( location.hash, '' ).replace(
+  		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+  		function( m, key, value ) { // callback
+  			vars[key] = value !== undefined ? value : '';
+  		}
+  	);
+
+
+  	if ( param ) {
+  		return vars[param] ? vars[param] : null;
+  	}
+  	return vars;
+  }
+
+  var postId = getQueryParam('postId');
+*/
   render() {
     return (
       <div>
-        <Post />
+        {this.state.items.map(item=><li key={item.id}>{item.body}</li>)}
       </div>
     );
   }
